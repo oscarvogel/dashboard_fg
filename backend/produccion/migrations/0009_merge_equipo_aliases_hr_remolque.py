3a4f1e628a371266e104825ad270d53ed7506bdf
Oscar Vogel <oscarvogel@gmail.com>
2026-07-10 15:36:37 -0300

    Add migration merge for deploy branches

---
+++ backend/produccion/migrations/0009_merge_equipo_aliases_hr_remolque.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("produccion", "0008_equipo_aliases_fields"),
+        ("produccion", "0008_registroproduccion_hr_remolque"),
+    ]
+
+    operations = []
